9ea9b40cb14056a3351f4cc43282f930eb9d60ef
Shunsuke Mishima <kcte8dtmp@yahoo.co.jp>
2019-10-29 13:59:02 +0200

    switch width and height(adjust the order to PIL)

--- divide/divide.py
+++ divide/divide.py
@@ -21,18 +21,18 @@ def divide(image, divide_size=(16, 16)):
     the list of divided image
   """
   im = np.array(Image.open(image))
-  (image_width, image_height,channels) = im.shape
-  divide_width, divide_height = divide_size
+  (image_height, image_width,channels) = im.shape
+  divide_height, divide_width = divide_size
 
   divide_num_x = int(image_width / divide_width)
   divide_num_y = int(image_height / divide_height)
 
   divided = []
-  for i in range(divide_num_x):
-    for j in range(divide_num_y):
-      x = i * divide_width
-      y = j * divide_height
-      divided.append(im[x:x+divide_width,y:y+divide_height,:])
+  for i in range(divide_num_y):
+    for j in range(divide_num_x):
+      y = i * divide_height
+      x = j * divide_width
+      divided.append(im[y:y+divide_height,x:x+divide_width,:])
 
   divided = np.array(divided)
   return divided
@@ -55,19 +55,19 @@ def combine(images, divide_size=(16, 16), image_size=(192, 256, 4)):
   combined_image : numpy.ndarray
     combined image
   """
-  (image_width, image_height,channels) = image_size
-  divide_width, divide_height = divide_size
+  (image_height, image_width,channels) = image_size
+  divide_height, divide_width = divide_size
 
   divide_num_x = int(image_width / divide_width)
   divide_num_y = int(image_height / divide_height)
 
   combined_image = np.zeros(image_size)
-  for i in range(divide_num_x):
-    for j in range(divide_num_y):
-      x = i * divide_width
-      y = j * divide_height
+  for i in range(divide_num_y):
+    for j in range(divide_num_x):
+      y = i * divide_height
+      x = j * divide_width
       part = images[0]
-      combined_image[x:x+divide_width,y:y+divide_height,:] = part
+      combined_image[y:y+divide_height,x:x+divide_width,:] = part
       images = np.delete(images, 0, 0)
 
   return combined_image
